452dd8554f0bb964c809ca081764f63a433ddbbe
chengong <654450188@qq.com>
2017-10-15 12:30:11 +0800

    dist打包

--- youzan/dist/static/js/member.cbbfe552e0e56d188141.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([5],{26:function(n,e){},27:function(n,e){},53:function(n,e,t){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var c=t(27),o=(t.n(c),t(26));t.n(o)}},[53]);
-//# sourceMappingURL=member.cbbfe552e0e56d188141.js.map
